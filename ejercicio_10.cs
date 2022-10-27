c2c17bf2ce747a7cb026a545921772f49c7e8309
F4t1m47 <116607908+F4t1m47@users.noreply.github.com>
2022-10-27 02:16:45 -0500

    cuadernillo

---
+++ ejercicio_10.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace EJERCICIO_10
+{
+    public static class program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Escribe el precio del producto");
+            double precio = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Escribe la forma de pago: tarjeta o efectivo");
+            String forma = (Console.ReadLine()).ToLower();
+
+            if (forma.Equals("tarjeta"))
+            {
+
+                Console.WriteLine("Introduce el numero de tarjeta");
+                int numero_cuenta = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("El producto con precio " + precio + " se ha pagado " +
+                "con el numero de cuenta " + numero_cuenta);
+
+
+            }
+            else if (forma.Equals("efectivo"))
+            {
+
+                Console.WriteLine("El producto con precio " + precio + " se ha pagado");
+
+            }
+            else
+            {
+
+                Console.WriteLine("La forma de pago no es correcta");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ejercicio_9.cs
@@ -0,0 +1,23 @@
+﻿namespace examen_1
+{
+    public static class program
+    {
+        public static void Main()
+        {
+            float salario;
+            Console.WriteLine("ingrese su salario");
+            salario = float.Parse(Console.ReadLine());
+            if (salario > 2000)
+            {
+                Console.WriteLine(" su nuevo salario es: " + salario * 10);
+            }
+            else 
+            
+            {
+                Console.WriteLine("su nuevo salario es: " + salario * 5);
+            }
+            Console.ReadKey();
+                
+        }
+    }
+}
